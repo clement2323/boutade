3ff0e9dd4f1d0199c0b641262ce18fa7fe061ec0
Guillo Clément <RK09OA@ad.insee.intra>
2024-11-04 21:10:17 -0400

    test validé

--- travail_sur_package.R
+++ travail_sur_package.R
@@ -1,10 +1,11 @@
-install.packages("devtools")
-devtools::document()
-devtools::check()
-devtools::load_all()
-getwd()
-setwd('codes/BoutadE')
-usethis::use_package('openxlsx')
+Sys.setenv(no_proxy = "")
+Sys.setenv(https_proxy ="http://proxy-rie.http.insee.fr:8080")
+Sys.setenv(http_proxy ="http://proxy-rie.http.insee.fr:8080")
+
+library(pbapply)
+
+#devtools::document()
+#devtools::check()
 
 [System.Net.WebRequest]::DefaultWebProxy = New-Object System.Net.WebProxy("http://proxy-rie.http.insee.fr:8080")
 $env:http_proxy = "http://proxy-rie.http.insee.fr:8080"
@@ -15,6 +16,7 @@ $env:no_proxy = ""
 # Mettre tous les éléments de la liste dans l'environnement global
 rm(list=ls())
 
+devtools::load_all()
 EP_FI_AG <-creer_table_minimale(20)
 table_demandes <-(creer_tables_demandes(EP_FI_AG))$table_demandes_valides
 #table_demandes <-(creer_tables_demandes(EP_FI_AG))$table_demandes_erreurs
@@ -28,9 +30,8 @@ for (i in 1:nrow(table_demandes)){
     gerer_une_demande(table_demandes[i,]%>% unlist())
 }
 
-out <- apply(table_demandes,1,gerer_une_demande)
+out <- pbapply(table_demandes,1,gerer_une_demande)
 generer_markdown_auto(table_demandes,out)
-
 
 rmarkdown::render(
     input = "output/rapport_automatique.Rmd",
